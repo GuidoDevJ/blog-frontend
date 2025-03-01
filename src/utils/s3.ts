f1eeebb3e43369f6db3ea7c591b40384dce065a4
Guido Gauna <guidogauna9@gmail.com>
2025-03-01 13:42:07 -0300

    change variable name

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -2,10 +2,10 @@ import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Readable } from 'stream';
 
 const s3 = new S3Client({
-  region: process.env.REGION,
+  region: process.env.NEXT_PUBLIC_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
 
