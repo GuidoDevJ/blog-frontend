6d4c2ac0f2e3115498b75ed36b2acdc45f29e116
Guido Gauna <guidogauna9@gmail.com>
2025-03-12 00:35:38 -0300

    add dark mode button

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import DarkModeButton from '@/components/Buttons/DarkMode';
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import getPost from '@/utils/request/getPost';
@@ -14,29 +15,32 @@ type Props = {
   params: Promise<Params>;
 };
 
-
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const slug= (await params).slug
+  const slug = (await params).slug;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [post, id] = slug.split("*");
+  const [post, id] = slug.split('*');
 
-  const dataPost = await getPost(id)
+  const dataPost = await getPost(id);
   return {
     title: `${dataPost?.title}`,
-    description: `Contenido del post ${dataPost?.title}, relacionados a ${dataPost?.tags.join(",")}`,
+    description: `Contenido del post ${
+      dataPost?.title
+    }, relacionados a ${dataPost?.tags.join(',')}`,
     openGraph: {
       title: `${dataPost?.title}`,
-      description: `Contenido del post ${dataPost?.title}, relacionados a ${dataPost?.tags.join(" , ")}`,
-      images:[dataPost?.image as string],
+      description: `Contenido del post ${
+        dataPost?.title
+      }, relacionados a ${dataPost?.tags.join(' , ')}`,
+      images: [dataPost?.image as string],
       type: 'article',
     },
   };
 }
 
 export default async function MarkdownPage({ params }: Props) {
-  const slug= (await params).slug
+  const slug = (await params).slug;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [postId,...other]= slug.split("*");
+  const [postId, ...other] = slug.split('*');
   const bucketName = process.env.NEXT_PUBLIC_BUCKET_NAME as string;
   const key = `${postId}.md`;
 
@@ -59,7 +63,12 @@ export default async function MarkdownPage({ params }: Props) {
     return (
       <>
         <Header />
-        <div className={`${styles.img} w-[90%] md:w-[50%] font-sans mx-auto dark:text-[#fff]`}>
+        <div className="w-[90%] sm:w-[96%] mx-auto flex justify-end mt-2">
+          <DarkModeButton />
+        </div>
+        <div
+          className={`${styles.img} w-[90%] md:w-[50%] font-sans mx-auto dark:text-[#fff]`}
+        >
           <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
         </div>
         <Footer />
@@ -67,7 +76,10 @@ export default async function MarkdownPage({ params }: Props) {
     );
   } catch (error) {
     // Manejo de errores al obtener el archivo Markdown
-    console.error(`Error fetching Markdown file from bucket ${bucketName} with key ${key}:`, error);
+    console.error(
+      `Error fetching Markdown file from bucket ${bucketName} with key ${key}:`,
+      error
+    );
     return <div>Markdown file not found</div>;
   }
 }
