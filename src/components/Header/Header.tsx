04b8e40072038cec3e02219354ece50811f481ff
Guido Gauna <guidogauna9@gmail.com>
2025-03-12 00:31:46 -0300

    refactor search bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,38 @@
-"use client"
-import usePostState from '@/store/posts.state';
-import Image from 'next/image';
+'use client';
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+
+import usePostState from "@/store/posts.state";
 import Search from '../../../public/search.svg';
 import SearchBar from '../SearchBar/SearchBar';
 
 const Header = () => {
-  const posts = usePostState((state)=>state.posts);
+  const posts = usePostState((state) => state.posts);
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
-    <header className="flex justify-end bg-primary text-white p-4">
-      <Image src={Search} alt="Search icon" width={50} height={50} />
-    <SearchBar posts={posts}/>
+    <header className="flex justify-end bg-primary text-white p-4 items-center">
+      
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        animate={{ opacity: showSearch ? 1 : 0, x: showSearch ? 0 : 50 }}
+        transition={{ duration: 0.3, ease: "easeOut" }}
+        className={`relative ${showSearch ? "block" : "hidden"}`}
+      >
+        <SearchBar posts={posts} />
+      </motion.div>
+      <Image
+        src={Search}
+        alt="Search icon"
+        width={50}
+        height={50}
+        className="cursor-pointer"
+        onClick={() => setShowSearch((prev) => !prev)}
+      />
     </header>
   );
 };
 
 export default Header;
+
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -35,26 +35,26 @@ const SearchBar = ({ posts }: SearchBarBlog) => {
   };
 
   return (
-    <div className="relative w-64 text-black">
+    <div className="w-[350px] text-black">
       <input
         type="text"
         value={query}
         onChange={handleChange}
-        className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-        placeholder="Search..."
+        className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 dark:text-white dark:border-none"
+        placeholder="Search"
       />
       {query && filteredSuggestions.length === 0 ? (
-        <ul className="absolute w-full bg-white border border-t-0 rounded-b-md shadow-lg z-10">
+        <ul className="absolute w-full bg-white border border-t-0 rounded-b-md shadow-lg dark:bg-[#121212] dark:text-white dark:border-none z-10">
           <li className="p-2 text-gray-500">No hay resultados</li>
         </ul>
       ) : (
         filteredSuggestions.length > 0 && (
-          <ul className="absolute w-full bg-white border border-t-0 rounded-b-md shadow-lg z-10">
+          <ul className="absolute w-full bg-white border border-t-0 rounded-b-md shadow-lg dark:bg-[#121212] dark:text-white dark:border-none z-10">
             {filteredSuggestions.map((post, index) => (
               <li
                 key={index}
                 onClick={() => handleSuggestionClick(post.title, post)}
-                className="p-2 cursor-pointer hover:bg-gray-200"
+                className="p-2 cursor-pointer hover:bg-gray-200 dark:hover:bg-inherit"
               >
                 {post.title}
               </li>
